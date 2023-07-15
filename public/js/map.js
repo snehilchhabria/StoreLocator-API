807c2da911f4e1a2e4dfa2ea4bd7bba4469bf0f1
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-07-15 14:17:07 +0530

    map setup

---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic25laGlsMjAyNCIsImEiOiJjbGsxYTV1cXYwNXh2M2Nqa21sa2praWNrIn0.M7F2zdHXNHOJz-lpeTqYww';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 9,
+    center: [77.216721, 28.644800]
+  });
+
--- routes/controllers/stores.js
+++
@@ -1,7 +0,0 @@
-// @desc Get all stores
-//@route Get /api/v1/stores
-//@access Public
-
-exports.getStores = (req,res,next) => {
-    res.send('Hello');
-}
